c709be44c4537c901d1e043758cac0631abda550
Aleksandr <komptver@gmail.com>
2022-12-14 12:26:27 +0300

    change integer to  double

--- Example003_SumNumbers/Program.cs
+++ Example003_SumNumbers/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 5;
-int numberB = 7;
-int numberC = 25;
-int result = numberC - (numberA + numberB);
+﻿Console.WriteLine("Введите первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+double result = (a+b);
+Console.Write("Сумма двух чисел равна "); 
 Console.WriteLine(result);
